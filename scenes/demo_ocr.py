a611025b4c28dc053fa7ba26c25b05472ef7e7e0
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2023-07-14 20:44:11 +0200

    OCR demo

--- scenes/demo_ocr.py
+++ scenes/demo_ocr.py
@@ -7,17 +7,30 @@ from random import choice
 
 class DemoOcr(NNScene):
   def construct(self):
+    self.potentialAxes = None
+    self.line = None
+    self.labels = None
     self.hopfieldNet = HopfieldNet(196)
     self.nn = LinearNN(self.hopfieldNet, 14, 14, 2.5, 0.19, False)
     self.g = self.nn.getGraph().to_edge(RIGHT)
 
     titleTxt = Text("Real-world demo").to_edge(UP)
-    self.play(Write(titleTxt))
+    learningTxt = Text("Learning", font_size = 30).next_to(titleTxt, DOWN)
+    inferringTxt = Text("Inferring", font_size = 30).next_to(titleTxt, DOWN)
+
+    self.play(
+      Write(titleTxt)
+    )
     self.wait()
     self.updateGraph()
     self.wait()
     img = self.fromNumbersToCanvas(2)
-    self.play(FadeIn(img))
+    caption = Text("*These patterns are not (too) overlapping", font_size = 20).next_to(img, DOWN)
+    self.play(
+      FadeIn(img),
+      FadeIn(caption),
+      Write(learningTxt)
+    )
     self.wait()
 
     LEARNED_PATTERNS = [2, 5, 7]
@@ -32,11 +45,12 @@ class DemoOcr(NNScene):
       self.wait()
       [_ for _ in self.hopfieldNet]  # learn
     
-    self.play(FadeOut(img))
+    self.play(FadeOut(img), FadeOut(caption))
 
+    self.play(Transform(learningTxt, inferringTxt))
+    self.wait()
     for num in LEARNED_PATTERNS:
-      self.copeWithNoiseSequence(num)
-    
+      self.copeWithNoise(num)
 
   def halveResolution(self, img: np.ndarray) -> np.ndarray:
     return img[::2, ::2]
@@ -57,7 +71,7 @@ class DemoOcr(NNScene):
     img.set_resampling_algorithm(RESAMPLING_ALGORITHMS["box"])
     return img.scale(40).to_edge(LEFT)
 
-  def copeWithNoiseSequence(self, n: int):
+  def copeWithNoise(self, n: int):
     imgN = self.fromNumbers(n)
     
     NOISE_RATE_ZERO = 0.30
@@ -71,6 +85,36 @@ class DemoOcr(NNScene):
     self.hopfieldNet.withMode(NetworkMode.INFER).withPattern(imgN)
     self.updateGraph()
     self.wait()
-    [_ for _ in self.hopfieldNet]
+    count = 0
+    t = []
+    E = []
+    for _ in self.hopfieldNet:
+      count += 1
+      t.append(count)
+      E.append(self.hopfieldNet.computeEnergy())
+    
+    if self.potentialAxes is not None:
+      self.remove(self.potentialAxes)
+      self.remove(self.line)
+      self.remove(self.labels)
+    
+    self.potentialAxes = Axes(
+      x_range = [0, t[-1], int(t[-1]/5)],
+      y_range = [0, E[-1], int(t[-1]/5)],
+      x_length = 5,
+      y_length = 6,
+      x_axis_config={"include_numbers": True},
+      y_axis_config={"include_numbers": True},
+      axis_config={"include_numbers": True},
+    ).to_edge(LEFT)
+    self.labels = self.potentialAxes.get_axis_labels(
+      Text("Iteration").scale(0.6),
+      Text("Energy").scale(0.6)
+    )
+    self.line = self.potentialAxes.plot_line_graph(t, E, add_vertex_dots=False)
+
     self.updateGraph()
-    self.wait()
+    self.add(self.potentialAxes, self.labels)
+    self.play(Create(self.potentialAxes))
+    self.add(self.potentialAxes, self.line)
+    self.wait(2)
