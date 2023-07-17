b129ad307af1ee1f1885c5e46fa6886c5e682b53
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2023-07-17 11:06:44 +0200

    first infra scene

---
+++ scenes/infra1.py
@@ -0,0 +1,25 @@
+from manim import *
+
+class Infra1(Scene):
+  def construct(self):
+    Text.set_default(font_size = 22)
+    r1 = Text(
+      """
+      A Hopfield network is a trivial kind of NN whose main application is pattern reconstruction.
+      """
+    ).shift(UP)
+    r2 = Text(
+      """
+      Like any other network it has to been submitted to a learning phase.
+      """
+    ).next_to(r1, DOWN, buff=0.2)
+    r3 = Text(
+      """
+      Two updating strategies are generally available: synchronous and asynchronous updating;
+      in this example, a synchronous neuron updating has been chosen.
+      """
+    ).next_to(r2, DOWN, buff=0.2)
+    self.play(LaggedStart(*[Write(r) for r in (r1, r2, r3)], lag_ratio = 0.7))
+    self.wait(5)
+    self.play(*[FadeOut(r) for r in (r1, r2, r3)])
+    self.wait(.5)
