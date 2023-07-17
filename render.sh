f620a5428613d26c2e58646aa18f9dce07a85406
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2023-07-17 11:37:12 +0200

    rendering script

---
+++ render.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+python3 -m manim -pqh scenes/infra1.py &
+python3 -m manim -pqh scenes/infra2.py &
+python3 -m manim -pqh scenes/infra3.py &
+python3 -m manim -pqh scenes/demo_nn.py &
+python3 -m manim -pqh scenes/demo_ocr.py --disable_caching &
+python3 -m manim -pqh scenes/nn_scene.py
