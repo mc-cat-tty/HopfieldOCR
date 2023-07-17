d435b9c022dcc5de9d7ed3fa26de9237eb43b893
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2023-07-17 02:35:50 +0200

    kickstart descending ball

--- scenes/energy_landscape.py
+++ scenes/energy_landscape.py
@@ -2,6 +2,7 @@ from manim import *
 from graphics.linear_nn import *
 from lib.hopfield import *
 from nn_scene import *
+from scipy.interpolate import *
 
 # Toroidal topology
 CARTESIAN_CHECKERBOARD_MAPPING = {  # (x, y)
@@ -72,14 +73,13 @@ class EnergyLandscape(ThreeDScene):
       z_length = 5
     )
 
-    surface = Surface(
+    self.surface = Surface(
       func = lambda x, y: axes.c2p(*paramSurface(x, y, self.hopfieldNet)),
       u_range = (-2, 2),
       v_range = (-2, 2),
       resolution = (4, 4)
     ).set_fill_by_value(axes = axes, colorscale=[(RED_C, -0.5), (YELLOW_C, 0), (GREEN_C, 0.5)], axis=2)
     z_label = axes.get_z_axis_label(Tex("Potential energy"))
-
     self.set_camera_orientation(zoom = 0.4)
 
     graphs = []
@@ -94,7 +94,8 @@ class EnergyLandscape(ThreeDScene):
           .shift(
             RIGHT * 1.35 * coord[0] +
             UP * 1.35 * coord[1]
-          )
+          ) \
+          .set_z_index(0)
         )
     self.hopfieldNet.withPattern(prevPattern)
 
@@ -113,86 +114,30 @@ class EnergyLandscape(ThreeDScene):
       Write(title),
       Write(hammingDistCaption)
     )
-    # self.wait(0.5)
+    self.wait(0.5)
 
-    # First pattern
+    #First pattern
     self.move_camera(phi=50 * DEGREES, theta=-90 * DEGREES, zoom=0.8, run_time=1.5)
-    self.begin_ambient_camera_rotation(90*DEGREES/5, about='theta')
-    self.begin_ambient_camera_rotation(90*DEGREES/80, about='phi')
-  
-    
-    footerTxt = Text("Learning a pattern is the process of modelling the energy landscape of the network", font_size = 30).to_edge(DOWN).shift(DOWN*0.4)
-    self.add_fixed_in_frame_mobjects(self.g.to_edge(RIGHT))
-    self.add_fixed_in_frame_mobjects(footerTxt)
-    self.play(
-      FadeIn(surface),
-      Write(footerTxt)
-    )
-    # self.wait(0.5)
+    self.begin_ambient_camera_rotation(90*DEGREES/21, about='theta')
+    self.begin_ambient_camera_rotation(90*DEGREES/120, about='phi')
 
-    for _ in self.hopfieldNet \
-      .withMode(NetworkMode.LEARN) \
-      .withPattern([[
-        1, 0,
-        0, 1
-      ]]):  # Learning
-      self.remove(surface, axes)
-      
-      axes = ThreeDAxes(
-        x_range = [-2.5, 2.5],
-        x_length = 7,
-        y_range = [-2.5, 2.5],
-        y_length = 7,
-        z_range = [-5, 5],
-        z_length = 5
-      )
-      
-      surface = Surface(
-        func = lambda x, y: axes.c2p(*paramSurface(x, y, self.hopfieldNet)),
-        u_range = (-2, 2),
-        v_range = (-2, 2),
-        resolution = (4, 4)
-      ) \
-      .set_fill_by_value(axes = axes, colorscale=[(RED_C, -0.5), (YELLOW_C, 0), (GREEN_C, 0.5)], axis=2)
-      
-      self.remove(self.g)
-      self.g = self.nn.getGraph().to_edge(RIGHT)
-      self.add_fixed_in_frame_mobjects(self.g)
-      self.add(surface, axes)
-
-      # self.wait(2)
-    
-    # self.wait(5)
-
-    ball_path = FunctionGraph(lambda x: 2, x_range = (0, 2)).rotate(90*DEGREES)
-    ball = Sphere(radius = 0.3, color = BLUE).move_to(ball_path[0])
-    self.play(GrowFromCenter(ball))
-    self.play(
-      MoveAlongPath(ball, ball_path),
-      run_time = 3,
-      rate_function = linear
-    )
-    self.wait(5)
-    
-    # Second pattern
-    self.move_camera(phi=50 * DEGREES, theta=-90 * DEGREES, zoom=0.8, run_time=1.5)
-    self.begin_ambient_camera_rotation(90*DEGREES/5, about='theta')
-    self.begin_ambient_camera_rotation(90*DEGREES/80, about='phi')
-  
-    self.remove(self.g, surface)
-    self.hopfieldNet = HopfieldNet(4)
-    self.nn = LinearNN(self.hopfieldNet, 2, 2, 2, 0.8, True)
-    self.g = self.nn.getGraph().to_edge(RIGHT)
-    self.add_fixed_in_frame_mobjects(self.g)
-    surface = Surface(
+    self.surface = Surface(
       func = lambda x, y: axes.c2p(*paramSurface(x, y, self.hopfieldNet)),
       u_range = (-2, 2),
       v_range = (-2, 2),
       resolution = (4, 4)
     ) \
     .set_fill_by_value(axes = axes, colorscale=[(RED_C, -0.5), (YELLOW_C, 0), (GREEN_C, 0.5)], axis=2)
-    self.play(FadeIn(surface))
+
+    footerTxt = Text("Learning a pattern is the process of modelling the energy landscape of the network", font_size = 30).to_edge(DOWN).shift(DOWN*0.4)
+    self.add_fixed_in_frame_mobjects(self.g.to_edge(RIGHT))
+    self.add_fixed_in_frame_mobjects(footerTxt)
+    self.play(
+      FadeIn(self.surface),
+      Write(footerTxt)
+    )
     self.wait(0.5)
+
 
     for _ in self.hopfieldNet \
       .withMode(NetworkMode.LEARN) \
@@ -200,7 +145,7 @@ class EnergyLandscape(ThreeDScene):
         1, 1,
         1, 1
       ]]):  # Learning
-      self.remove(surface, axes)
+      self.remove(self.surface, axes)
       
       axes = ThreeDAxes(
         x_range = [-2.5, 2.5],
@@ -211,7 +156,7 @@ class EnergyLandscape(ThreeDScene):
         z_length = 5
       )
       
-      surface = Surface(
+      self.surface = Surface(
         func = lambda x, y: axes.c2p(*paramSurface(x, y, self.hopfieldNet)),
         u_range = (-2, 2),
         v_range = (-2, 2),
@@ -222,11 +167,93 @@ class EnergyLandscape(ThreeDScene):
       self.remove(self.g)
       self.g = self.nn.getGraph().to_edge(RIGHT)
       self.add_fixed_in_frame_mobjects(self.g)
-      self.add(surface, axes)
+      self.add(self.surface, axes)
 
       self.wait(2)
     
     self.wait(5)
 
+    # Second pattern
+    self.move_camera(phi=50 * DEGREES, theta=-90 * DEGREES, zoom=0.8, run_time=1.5)
+  
+    self.remove(self.g, self.surface)
+    self.hopfieldNet = HopfieldNet(4)
+    self.nn = LinearNN(self.hopfieldNet, 2, 2, 2, 0.8, True)
+    self.g = self.nn.getGraph().to_edge(RIGHT)
+    self.add_fixed_in_frame_mobjects(self.g)
+
+    self.surface = Surface(
+      func = lambda x, y: axes.c2p(*paramSurface(x, y, self.hopfieldNet)),
+      u_range = (-2, 2),
+      v_range = (-2, 2),
+      resolution = (4, 4)
+    ) \
+    .set_fill_by_value(axes = axes, colorscale=[(RED_C, -0.5), (YELLOW_C, 0), (GREEN_C, 0.5)], axis=2)
+    self.play(FadeIn(self.surface))
+    self.wait(0.5)
+    
+    for _ in self.hopfieldNet \
+      .withMode(NetworkMode.LEARN) \
+      .withPattern([[
+        1, 0,
+        0, 1
+      ]]):  # Learning
+      self.remove(self.surface, axes)
+      
+      axes = ThreeDAxes(
+        x_range = [-2.5, 2.5],
+        x_length = 7,
+        y_range = [-2.5, 2.5],
+        y_length = 7,
+        z_range = [-5, 5],
+        z_length = 5
+      )
+      
+      self.surface = Surface(
+        func = lambda x, y: axes.c2p(*paramSurface(x, y, self.hopfieldNet)),
+        u_range = (-2, 2),
+        v_range = (-2, 2),
+        resolution = (4, 4)
+      ) \
+      .set_fill_by_value(axes = axes, colorscale=[(RED_C, -0.5), (YELLOW_C, 0), (GREEN_C, 0.5)], axis=2)
+      
+      self.remove(self.g)
+      self.g = self.nn.getGraph().to_edge(RIGHT)
+      self.add_fixed_in_frame_mobjects(self.g)
+      self.add(self.surface, axes)
+
+      self.wait(2)
+    
+    self.wait(5)
+
+
+    self.rollBall1()
+
+
+  def rollBall1(self):
+    sPoints = self.surface.get_all_points()
+    f = interp2d(sPoints[:, 0], sPoints[:, 1], sPoints[:, 2], kind = 'cubic')
+    
+    ballRadius = 0.3
+    ballPath = FunctionGraph(
+      lambda x: f(x-3, 3)[0],
+      x_range = (0, 3)
+    ) \
+      .rotate(90 * DEGREES, axis = X_AXIS, about_point = (0, 0, 0)) \
+      .rotate(90 * DEGREES, axis = Z_AXIS, about_point = (0, 0, 0)) \
+      .rotate(90 * DEGREES, axis = Z_AXIS, about_point = (0, 3, 0)) \
+      .rotate(360 * DEGREES, axis = Z_AXIS) \
+      .shift((0, -ballRadius/2, ballRadius/2)) \
+      .set_shade_in_3d(True) \
+      .set_z_index(100)
+    ball = Sphere(radius = ballRadius, color = BLUE).move_to(ballPath[0]).set_z_index(100)
+    self.stop_ambient_camera_rotation(about = 'theta')
+    self.stop_ambient_camera_rotation(about = 'phi')
+    self.play(
+      MoveAlongPath(ball, ballPath),
+      run_time = 10,
+      rate_function = smooth
+    )
+    self.wait(10)
 
 
