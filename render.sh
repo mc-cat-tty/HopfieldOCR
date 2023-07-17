a3758831afc0d441d0b00fd5535bcecd6ec32aa2
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2023-07-17 19:05:53 +0200

    update render script with new scenes

--- render.sh
+++ render.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+python3 -m manim -pqh scenes/intro.py &
 python3 -m manim -pqh scenes/infra1.py &
 python3 -m manim -pqh scenes/infra2.py &
 python3 -m manim -pqh scenes/infra3.py &
 python3 -m manim -pqh scenes/demo_nn.py &
 python3 -m manim -pqh scenes/demo_ocr.py --disable_caching &
-python3 -m manim -pqh scenes/nn_scene.py
+python3 -m manim -pqh scenes/nn_scene.py &
+python3 -m manim -pqk scenes/energy_landscape.py
